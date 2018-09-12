2e6a092d1c912fb1ee6cffdc6db4b07743d56071
tleytek <lucidmat94@gmail.com>
2018-09-12 19:19:13 -0400

    fix package.json script

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,6 +5,13 @@
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var friendsData = [{}];
+var friendsData = [
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
 
 module.exports = tableArray;
