69e804d1005f2a6fc9307c95893a6cde467fac18
tleytek <lucidmat94@gmail.com>
2018-09-12 18:58:46 -0400

    added html and api routes

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsData = [{}];
+
+module.exports = tableArray;
