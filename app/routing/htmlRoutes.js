c8718b1bb9e2b5461624d7944d01e5c73ab5a661
tleytek <lucidmat94@gmail.com>
2018-09-12 20:23:06 -0400

    fixes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,11 +14,15 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "..public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var bodyParser = require("body-parser");
 var app = express();
 
 // Sets an initial port.
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8081;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
